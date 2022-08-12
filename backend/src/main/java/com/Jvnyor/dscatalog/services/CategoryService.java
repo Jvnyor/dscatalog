cbe9e8f07b41f947442130e31b9053943e24bb75
Jvnyor <josias100junior@gmail.com>
2022-08-12 00:18:36 -0300

    Update category

--- backend/src/main/java/com/Jvnyor/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/Jvnyor/dscatalog/services/CategoryService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.Jvnyor.dscatalog.dto.CategoryDTO;
 import com.Jvnyor.dscatalog.entities.Category;
 import com.Jvnyor.dscatalog.repositories.CategoryRepository;
-import com.Jvnyor.dscatalog.services.exceptions.EntityNotFoundException;
+import com.Jvnyor.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -29,13 +29,26 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		return new CategoryDTO(repository.findById(id)
-				.orElseThrow(() -> new EntityNotFoundException("Entity not found")));
+				.orElseThrow(() -> new ResourceNotFoundException("Entity not found")));
 	}
-
+	
+	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 }
