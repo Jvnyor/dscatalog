b5523dcb44093d3c534b1462bab34c5bf53f2438
Jvnyor <josias100junior@gmail.com>
2022-08-12 00:05:02 -0300

    POST new Category

--- backend/src/main/java/com/Jvnyor/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/Jvnyor/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.Jvnyor.dscatalog.dto.CategoryDTO;
+import com.Jvnyor.dscatalog.entities.Category;
 import com.Jvnyor.dscatalog.repositories.CategoryRepository;
 import com.Jvnyor.dscatalog.services.exceptions.EntityNotFoundException;
 
@@ -29,5 +30,12 @@ public class CategoryService {
 	public CategoryDTO findById(Long id) {
 		return new CategoryDTO(repository.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException("Entity not found")));
+	}
+
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
 	}
 }
