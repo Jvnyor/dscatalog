0ddf7f22aca00bf9be3f830c3a19220693a37d68
Jvnyor <josias100junior@gmail.com>
2022-08-24 15:25:20 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/Jvnyor/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/Jvnyor/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.Jvnyor.dscatalog.dto;
 
+import com.Jvnyor.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
